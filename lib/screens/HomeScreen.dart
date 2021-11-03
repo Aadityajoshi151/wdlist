e99648d39a3a37e38c40ae6315bef0ceb83f9176
Aaditya Joshi <aadityajoshi151@gmail.com>
2021-11-03 12:18:48 +0530

    changed structure and added FAB
    FAB is detecting the tab index onpress

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -3,32 +3,41 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
+  
 }
-
+const List<Tab> tabs = <Tab>[
+  Tab(text: 'Morning',icon: Icon(Icons.wb_sunny)),
+  Tab(text: 'Evening',icon: Icon(Icons.nights_stay)),
+  Tab(text: 'Weekend',icon: Icon(Icons.weekend)),
+];
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
+      length: tabs.length,
+
+      child: Builder(builder: (BuildContext context) {
+        final TabController tabController = DefaultTabController.of(context)!;
+        tabController.addListener(() {
+          if (!tabController.indexIsChanging) {
+          }
+        });
+        return Scaffold(
           appBar: AppBar(
+            title: Center(child: Text("WDList")),
             elevation: 0,
             bottom: const TabBar(
-              tabs: [
-                Tab(text: "Morning",icon: Icon(Icons.wb_sunny)),
-                Tab(text: "Evening",icon: Icon(Icons.nights_stay)),
-                Tab(text: "Weekend",icon: Icon(Icons.weekend)),
-              ],
+              tabs: tabs,
             ),
-            title: Center(child: const Text("WDList"),),
           ),
-          body: const TabBarView(
-            children: [
-              Icon(Icons.wb_sunny),
-              Icon(Icons.nights_stay),
-              Icon(Icons.weekend),
-            ],
+          body: TabBarView(
+            children: tabs.map((Tab tab) {
+              return Center(
+                child: Icon(Icons.wifi)
+              );
+            }).toList(),
           ),
-        ),
-      );
-  }}
+        );
+      }),
+    );}
+  }
