cc113b7c2c6ee1f9310b8f6b4796d6fdf7bddb6b
Aaditya Joshi <aadityajoshi151@gmail.com>
2021-11-03 18:52:55 +0530

    getting value from inputmodal

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -2,25 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:wdlist/widgets/inputmodal.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
   @override
   _HomeScreenState createState() => _HomeScreenState();
-  
 }
+
 const List<Tab> tabs = <Tab>[
-  Tab(text: 'Morning',icon: Icon(Icons.wb_sunny)),
-  Tab(text: 'Evening',icon: Icon(Icons.nights_stay)),
-  Tab(text: 'Weekend',icon: Icon(Icons.weekend)),
+  Tab(text: 'Morning', icon: Icon(Icons.wb_sunny)),
+  Tab(text: 'Evening', icon: Icon(Icons.nights_stay)),
+  Tab(text: 'Weekend', icon: Icon(Icons.weekend)),
 ];
+
+String task = "";
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: tabs.length,
-
       child: Builder(builder: (BuildContext context) {
         final TabController tabController = DefaultTabController.of(context)!;
-        
+
         return Scaffold(
           appBar: AppBar(
             title: Center(child: Text("WDList")),
@@ -33,27 +35,24 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           body: TabBarView(
-            
             children: tabs.map((Tab tab) {
-              return Center(
-                child: Icon(Icons.wifi)
-              );
+              return Center(child: Icon(Icons.wifi));
             }).toList(),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: (){
+            onPressed: () {
               showDialog(
-                context: context, 
-              builder: (BuildContext context){
-                return InputModal();
-              }
-              );
-              //print("Tab: "+tabs[tabController.index].text.toString());
+                  context: context,
+                  builder: (BuildContext context) {
+                    return InputModal();
+                  });
+            print(task);      
+            print("Tab: "+tabs[tabController.index].text.toString());
             },
             child: Icon(Icons.add),
           ),
         );
       }),
     );
-    }
   }
+}
--- lib/widgets/inputmodal.dart
+++ lib/widgets/inputmodal.dart
@@ -1,40 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:wdlist/screens/HomeScreen.dart';
 
 class InputModal extends StatefulWidget {
-  const InputModal({ Key? key }) : super(key: key);
-
+  
+  const InputModal({Key? key}) : super(key: key);
+  
   @override
   _InputModalState createState() => _InputModalState();
 }
 
 class _InputModalState extends State<InputModal> {
+  TextEditingController taskcontroller = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-                    scrollable: true,
-                    title: const Text('Enter The Task'),
-                    content: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Form(
-                        child: Column(
-                          children: <Widget>[
-                            TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Name',
-                                icon: Icon(Icons.account_box),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                     actions: [
-                      ElevatedButton(
-                          child: Text("Submit"),
-                          onPressed: () {
-                            print("Submit Pressed");
-                          })
-                    ],
-                  );
+      scrollable: true,
+      content: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: taskcontroller,
+              decoration: InputDecoration(
+                labelText: 'Enter Task',
+              ),
+            )
+          ],
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+            child: Text("Create"),
+            onPressed: () {
+              task = taskcontroller.text;
+              Navigator.of(context).pop();
+            })
+      ],
+    );
   }
 }
